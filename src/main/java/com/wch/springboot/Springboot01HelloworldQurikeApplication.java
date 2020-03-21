eb4d3bdbaeda0d15c6754aeceb4e3eae350c77ed
wch <257@qq.com>
2020-03-21 15:36:57 +0800

    第一次提交！

---
+++ src/main/java/com/wch/springboot/Springboot01HelloworldQurikeApplication.java
@@ -0,0 +1,13 @@
+package com.wch.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01HelloworldQurikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01HelloworldQurikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wch/springboot/Springboot01HelloworldQurikeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wch.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot01HelloworldQurikeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
